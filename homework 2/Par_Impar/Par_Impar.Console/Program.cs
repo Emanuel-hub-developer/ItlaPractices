c96ee20feef85ccbad34fa516a907f416e6bb759
Emanuel-hub-developer <emanuelbalbuena295@gmail.com>
2025-01-31 21:23:21 -0400

    uploading homework 2

---
+++ homework 2/Par_Impar/Par_Impar.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var answer = 0;
+
+        Console.WriteLine("Ingrese un numero: ");
+        answer = Convert.ToInt32(Console.ReadLine());
+
+        if (answer % 2 == 0)
+        {
+            Console.WriteLine("El numero es par.");
+        }
+        else
+        {
+            Console.WriteLine("El numero es impar.");
+        }
+    }
+}
