56735c05cb58ad9b3a1463ef40a95131580e729e
Emanuel-hub-developer <emanuelbalbuena295@gmail.com>
2025-03-19 01:10:35 -0400

    uploading homework 4

---
+++ homework 4/Calculator/Calculadora.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+   
+        class Calculadora
+        {
+            private List<decimal> typedNumbers = new List<decimal>();
+
+            public void DisplayHeader()
+            {
+                Console.WriteLine("Please Type the option number that you want");
+                Console.WriteLine("---------------------------------------");
+                Console.WriteLine("1. Sum, \n2. Subtract,  \n3. Multiplication,  \n4. Division,  \n5. Exit");
+            }
+
+            public decimal Sum()
+            {
+                decimal result = 0;
+                foreach (decimal number in typedNumbers)
+                {
+                    result += number;
+                }
+                return result;
+            }
+
+            public decimal Subtract()
+            {
+                if (typedNumbers.Count == 0) return 0;
+                decimal result = typedNumbers[0];
+                for (int i = 1; i < typedNumbers.Count; i++)
+                {
+                    result -= typedNumbers[i];
+                }
+                return result;
+            }
+
+            public decimal Multiply()
+            {
+                if (typedNumbers.Count == 0) return 0;
+                decimal result = 1;
+                foreach (decimal number in typedNumbers)
+                {
+                    result *= number;
+                }
+                return result;
+            }
+
+            public decimal Divide()
+            {
+                if (typedNumbers.Count == 0) return 0;
+                decimal result = typedNumbers[0];
+                for (int i = 1; i < typedNumbers.Count; i++)
+                {
+                    if (typedNumbers[i] == 0)
+                        throw new DivideByZeroException("Cannot divide by zero.");
+                    result /= typedNumbers[i];
+                }
+                return result;
+            }
+
+            public void ClearNumbers()
+            {
+                typedNumbers.Clear();
+            }
+
+            public void AddNumber(decimal number)
+            {
+                typedNumbers.Add(number);
+            }
+        }
+    }
+    
+
---
+++ homework 4/Calculator/Program.cs
@@ -0,0 +1,77 @@
+﻿using Calculator;
+
+Calculadora calc = new Calculadora();
+bool running = true;
+int option = 0;
+
+Console.WriteLine("This is the best calculator");
+
+while (running)
+{
+    calc.DisplayHeader();
+
+    try
+    {
+        option = Convert.ToInt32(Console.ReadLine());
+
+        if (option == 5)
+        {
+            running = false;
+            continue;
+        }
+
+        calc.ClearNumbers();
+
+        Console.WriteLine("Please Type the first number");
+        calc.AddNumber(Convert.ToDecimal(Console.ReadLine()));
+
+        Console.WriteLine("Please Type the second number");
+        calc.AddNumber(Convert.ToDecimal(Console.ReadLine()));
+
+        int wantToContinue = 0;
+        while (wantToContinue != 2)
+        {
+            Console.WriteLine("Do you want to continue inserting numbers? 1. Yes, 2. No");
+            wantToContinue = Convert.ToInt32(Console.ReadLine());
+            if (wantToContinue == 1)
+            {
+                Console.WriteLine("Please Type another number");
+                calc.AddNumber(Convert.ToDecimal(Console.ReadLine()));
+            }
+        }
+
+        decimal result = 0;
+        switch (option)
+        {
+            case 1:
+                result = calc.Sum();
+                break;
+            case 2:
+                result = calc.Subtract();
+                break;
+            case 3:
+                result = calc.Multiply();
+                break;
+            case 4:
+                result = calc.Divide();
+                break;
+            default:
+                Console.WriteLine("Invalid option.");
+                break;
+        }
+
+        Console.WriteLine($"The Result of the operation is: {result}");
+    }
+    catch (DivideByZeroException ex)
+    {
+        Console.WriteLine($"You cannot divide by zero: {ex.Message}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+    finally
+    {
+        Console.WriteLine("Closing DB Connection");
+    }
+}
